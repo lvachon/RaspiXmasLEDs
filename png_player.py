d1633f862d03529cc8601603e578327273aba332
lvachon <lvachon@gmail.com>
2019-12-03 21:34:17 -0500

    Multiple animation cycling

--- png_player.py
+++ png_player.py
@@ -12,26 +12,37 @@ num_pixels = 110
 ORDER = neopixel.GRB
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER)
-animation = []
-#BRG RGB
-f = open(sys.argv[1],"r")
-format = f.readline()
-comment = f.readline()
-dimensions = f.readline().split()
-maxval = f.readline()
-data = f.read()
-data = data.split()	
-for y in range(0,int(dimensions[1])):
-	animation.append([])
-	for x in range(0,int(dimensions[0])):
-		index = 3*(x+y*int(dimensions[0]))
-		animation[y].append((int(data[index+0]),int(data[index+1]),int(data[index+2])))
+animations = []
 
+for i in range(1,len(sys.argv)):
+	animations.append([])
+	f = open(sys.argv[1],"r")
+	format = f.readline()
+	comment = f.readline()
+	dimensions = f.readline().split()
+	maxval = f.readline()
+	data = f.read()
+	data = data.split()
+	for y in range(0,int(dimensions[1])):
+		animations[i].append([])
+		for x in range(0,int(dimensions[0])):
+			index = 3*(x+y*int(dimensions[0]))
+			animations[i][y].append((int(data[index+0]),int(data[index+1]),int(data[index+2])))
+
+frameIndex=0
 animIndex=0
+animCount=len(sys.argv)-1
+loopCount=0
+maxLoops=4
 while(True):
 	for x in range(0,int(dimensions[0])):
-		pixels[x]=animation[animIndex][x]
+		pixels[x]=animations[animIndex][frameIndex][x]
 	pixels.show()
-	animIndex = (animIndex+1)%int(dimensions[1])
-	time.sleep(0.06)
+	frameIndex = (frameIndex+1)%int(dimensions[1])
+	if(frameIndex==0):
+		loopCount=loopCount+1
+		if(loopCount==5):
+			loopCount=0
+			animIndex=(animIndex+1)%animCount
+	time.sleep(0.05)
 	
