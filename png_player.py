d9eb61c95833edfc5e355b0e7d3c3e20a8488a56
lvachon <lvachon@gmail.com>
2019-12-03 20:53:14 -0500

    New image and stuff

--- png_player.py
+++ png_player.py
@@ -12,24 +12,26 @@ ORDER = neopixel.GRB
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER)
 animation = []
-
+#BRG RGB
 f = open("sparklesNribbons.ppm","r")
 format = f.readline()
 comment = f.readline()
 dimensions = f.readline().split()
+maxval = f.readline()
 data = f.read()
 data = data.split()	
-for y in range(0,dimensions[1]):
+for y in range(0,int(dimensions[1])):
 	animation.append([])
-	for x in range(0,dimensions[0]):
-		index = 3*(x+y*dimensions[0])
-		animation[y].append(data[index],data[index+1],data[index+2])
+	for x in range(0,int(dimensions[0])):
+		index = 3*(x+y*int(dimensions[0]))
+		animation[y].append((int(data[index+0]),int(data[index+1]),int(data[index+2])))
 
 animIndex=0
 while(True):
-	for x in range(0,dimensions[0]):
+	for x in range(0,int(dimensions[0])):
 		pixels[x]=animation[animIndex][x]
 	pixels.show()
-	animIndex = (animIndex+1)%dimensions[1]
+	animIndex = (animIndex+1)%int(dimensions[1])
+	print(animIndex)
 	time.sleep(0.03)
 	
